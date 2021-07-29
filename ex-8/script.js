26a672dd964bc6143a0a8dd85d058ae782faa2a3
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-07-29 00:51:51 -0300

    Arruma o exercicio pois tinha trocado com o nove

--- ex-8/script.js
+++ ex-8/script.js
@@ -6,7 +6,7 @@ let verificador_1 = a % 2;
 let verificador_2 = b % 2;
 let verificador_3 = c % 2;
 
-if (verificador_1 !=0 || verificador_2 !=0 ||verificador_3 !=0){
+if (verificador_1 ===0 || verificador_2 ===0 ||verificador_3 ===0){
     console.log(true);
 } else {
     console.log(false)
