ca105397ee8ff1cdb04ad12bcfd55841b3494974
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-07-29 00:49:54 -0300

    Realiza o exercicio 9

---
+++ ex-9/script.js
@@ -0,0 +1,14 @@
+let a = 20;
+let b = 15;
+let c = 10;
+
+let verificador_1 = a % 2;
+let verificador_2 = b % 2;
+let verificador_3 = c % 2;
+
+
+if(verificador_1 === 0 || verificador_2 === 0 || verificador_3 === 0){
+    console.log(true);
+} else {
+    console.log(false);
+}
