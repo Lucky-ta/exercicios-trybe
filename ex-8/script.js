1331b540fee6e517c244465916a12371f618912c
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-02 20:12:42 -0300

    Realiza o oitavo exercicio

---
+++ ex-8/script.js
@@ -0,0 +1,8 @@
+//Utilizando for, crie um array que vá de 1 ate 25 e imprima o resultado;
+
+let lista = [];
+
+for (let index =0; index < 25; index+=1) {
+    lista.push(index+1)
+}
+console.log(lista);
