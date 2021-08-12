75361306eba7b11e42d7d75ffd43b91f1df12620
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-11 22:23:35 -0300

    Finaliza o dia

--- ex-11/script.js
+++ ex-11/script.js
@@ -1,31 +1,31 @@
-let salario_bruto = 3000;
-let salario_com_desconto = 0;
-let salario_liquido = 0;
+//exerciico feito com base no gabarito
+let aliquotINSS;
+let aliquotIR;
 
-if (salario_bruto <= 1.556,94) {
-    salario_com_desconto = ((salario_bruto) - (salario_bruto* 8/100));
-} else if (salario_bruto === 1.556,95 && salario_bruto <= 2.259,92) {
-    salario_com_desconto = ((salario_bruto) - (salario_bruto* 9/100));
-} else if (salario_bruto === 2.259,93 && salario_bruto <= 5.189,82) {
-    salario_com_desconto = ((salario_bruto) - (salario_bruto* 11/100));
-} else if (salario_bruto > 5.189,82) {
-    salario_com_desconto = (salario_bruto - 570.88);
+const grossSalary = 2000.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
 }
-console.log(salario_com_desconto)
 
+const baseSalary = grossSalary - aliquotINSS;
 
-if (salario_com_desconto <= 1903.98) {
-    console.log('Voce está isento de imposto.');
-} else if (salario_com_desconto === 1903.99 && salario_bruto <= 2826.65) {
-    salario_liquido = ((salario_com_desconto) - ((salario_com_desconto* 7.5/100) - 142.80))
-    console.log(salario_liquido)
-} else if ( salario_com_desconto === 2826.66 && salario_bruto <= 3751.05) {
-    salario_liquido = ((salario_com_desconto) - ((salario_com_desconto* 15/100) - 354.80))
-    console.log(salario_liquido)
-} else if ( salario_com_desconto === 3751.06 && salario_bruto <= 4664.68) {
-    salario_liquido = ((salario_com_desconto) - ((salario_com_desconto* 22.5/100) - 636.13))
-    console.log(salario_liquido)
-} else if (salario_com_desconto > 4664.68) {
-    salario_liquido = ((salario_com_desconto) - ((salario_com_desconto* 27.5/100) - 869.36))
-    console.log(salario_liquido)
-}
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
