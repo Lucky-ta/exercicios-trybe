72c12e2e693a61e29e9e7c3f7d9e6b49d066eeb3
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-20 19:34:24 -0300

    Realiza primeiros exercicios de pratica

--- exercicios-0.1/main.js
+++ exercicios-0.1/main.js
@@ -23,14 +23,23 @@ thirdLi.addEventListener('click', add_class_tech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-
+input.addEventListener('input', function(event){
+  const tech_element = document.querySelector('.tech');
+  tech_element.innerText = event.target.value;
+})
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function(){
+  window.location.replace('https://github.com/Lucky-ta');
+})
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', function(event){
+  event.target.style.color = 'purple'
+})
 
 // Segue abaixo um exemplo do uso de event.target:
 
