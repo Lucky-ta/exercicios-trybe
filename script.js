48951a1a1ef2dfb7e14a79a9984a5074c86061e6
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-09-03 00:04:53 -0300

    Descomenta os codigos

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // Inicio do primeiro requisito;
-// let returnObject = (nome) => {
-//     return {
-//         nomeCompleto: nome,
-//         emailCompleto: `${nome}@gmail.com`
-//     }
-// }
+let returnObject = (nome) => {
+    return {
+        nomeCompleto: nome,
+        emailCompleto: `${nome}@gmail.com`
+    }
+}
 
-// const newEmployees = (callBack) => {
-//     const employees = {
-//         id1: callBack('Pedro Guerra'),
-//         id2: callBack('Luiza Drumond'),
-//         id3: callBack('Carla Paiva'),
-//     }
-//     return employees;
-// };
+const newEmployees = (callBack) => {
+    const employees = {
+        id1: callBack('Pedro Guerra'),
+        id2: callBack('Luiza Drumond'),
+        id3: callBack('Carla Paiva'),
+    }
+    return employees;
+};
 
-// console.log(newEmployees(returnObject));
-// // Fim do primeiro requisito.
+console.log(newEmployees(returnObject));
+// Fim do primeiro requisito.
 
-// // Inicio do segundo requisito;
-// const checkNumber = (inputNumber, random) => inputNumber === random;
+// Inicio do segundo requisito;
+const checkNumber = (inputNumber, random) => inputNumber === random;
 
-// const resultado = (inputNumber, callBack) => {
-//     const random = Math.floor(Math.random() * 6);
+const resultado = (inputNumber, callBack) => {
+    const random = Math.floor(Math.random() * 6);
 
-//     return callBack(inputNumber, random) ? 'Parabens, voce ganhou' : 'Tente novamente';
-// }
+    return callBack(inputNumber, random) ? 'Parabens, voce ganhou' : 'Tente novamente';
+}
 
-// console.log(resultado(3, checkNumber));
+console.log(resultado(3, checkNumber));
 // Fim do segundo requisito.
 
 // Inicio do terceiro requisito;
@@ -52,3 +52,4 @@ console.log(nota(RIGHT_ANSWERS, STUDENT_ANSWERS, (rCerta, rEstudante)=> {
     }
     return -0.5;
 }));
+// Fim do terceiro requisito.
